3c2650d357a66f16152e944be7d69cbd68c2ffa4
Scott <hscottvo@gmail.com>
2021-04-07 00:18:34 -0700

    Added initial turnin directory

---
+++ turnin/svo021_lab2_part1.c
@@ -0,0 +1,36 @@
+/*	Author: svo021
+ *  Partner(s) Name: Scott Vo
+ *	Lab Section:021
+ *	Assignment: Lab #2  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; // Configure 8 pins of A into inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure 8 pins of B into outputs, initialize to 0s
+    /* Insert your solution below */
+	unsigned char tmpB = 0x00;
+	unsigned char tmpA = 0x00;	
+
+    while (1) {
+	// read input
+	tmpA = PINA & 0x01;
+	// computation: if PA0 == 1 -> set PB1PB0 = 01, else = 10
+	if (tmpA == 0x01) {
+		tmpB = (tmpB & 0xFC) | 0x01;
+	}
+	else {
+		tmpB = (tmpB & 0xFC) | 0x02;
+	}
+	PORTB = tmpB;
+    }
+    return 0;
+}
