2415545c5046a2063808831e34658c4d74f6d96d
Scott Vo <svo021@wch136-24.cs.ucr.edu>
2021-04-07 22:06:22 -0700

    Added Part 3 files

---
+++ turnin/svo021_lab2_part3.c
@@ -0,0 +1,41 @@
+/*	Author: svo021
+ *  Partner(s) Name: Scott Vo
+ *	Lab Section:021
+ *	Assignment: Lab #2  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; // Configure 8 pins of A into inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure 8 pins of B into outputs, initialize to 0s
+    /* Insert your solution below */
+	unsigned char temp = 0;
+	unsigned char tempA = 0x00;
+	unsigned char i = 0;
+
+    while (1) {
+	// read input
+	// Computation: count number of 0 bits in A3:A0
+	tempA = PINA & 0x0F;
+	temp = 0x00;
+	for (i = 0; i < 4; ++i) {
+	    if ((tempA & 0x01) == 0x00){
+		temp = temp + 1;
+	    } 
+	    tempA = tempA >> 1;
+        }
+  	if (temp == 0x00) {
+	    temp = temp + 0x80;	
+	}	
+	PORTC = temp;
+    }
+    return 0;
+}
